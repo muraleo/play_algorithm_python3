1c7e5f3e230a6799fcb5010bbee611495a1a9fa2
muraleo <muraleo80923@gmail.com>
2018-06-29 20:03:21 -0400

    create recursive binary search

--- binarySearch/binarySearch.py
+++ binarySearch/binarySearch.py
@@ -2,9 +2,9 @@ def binarySearchIter(arr, target):
     if not arr:
         return None
     # search in [l,r]
-    l = 0, r = len(arr)-1
+    l, r = 0, len(arr)-1
     while(l <= r):
-        int mid = l + (r-l)//2
+        mid = l + (r-l)//2
         if(arr[mid] < target):
             l = mid + 1
         elif(arr[mid] > target):
@@ -12,3 +12,19 @@ def binarySearchIter(arr, target):
         else:
             return mid
     return None
+
+def  __binarySearchRec(arr, l, r, target):
+    if not arr:
+        return None
+    mid = l + (r - l)//2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return __binarySearchRec(arr, mid+1, r, target)
+    else:
+        return __binarySearchRec(arr, l, mid-1, target)
+
+def binarySearchRec(arr, target):
+    if not arr:
+        return None
+    return __binarySearchRec(arr, 0, len(arr)-1, target)
